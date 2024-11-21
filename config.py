0e1e94384e4196a3b14f484f0ade32cee448bee8
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-21 19:09:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 7009601543))
+OWNER_ID = getenv("OWNER_ID", 7009601543)
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PBX_CHAT")
 
 MUST_JOIN = getenv("MUST_JOIN", "https://t.me/PBX_CHAT")
