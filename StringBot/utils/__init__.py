2fb8040cc4d9d38a7918d0d07f2215350209442f
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-21 19:47:12 +0530

    Update __init__.py

--- StringBot/utils/__init__.py
+++ StringBot/utils/__init__.py
@@ -1,2 +1,10 @@
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+import config
+
+
+mongo = MongoCli(config.MONGO_DB_URI)
+db = mongo.StringGen
+
 from .inline import *
 from .users import *
