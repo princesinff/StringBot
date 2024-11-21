40c65d969b2c09f1ea1b197acc6f4849795eb02f
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-11-21 17:54:42 +0530

    Update callbacks.py

--- StringBot/modules/callbacks.py
+++ StringBot/modules/callbacks.py
@@ -1,29 +1,41 @@
-from pyrogram import filters
-from pyrogram.types import CallbackQuery
+import traceback
 
-from StringBot import Bad
-from StringBot.utils import gen_key
-from StringBot.modules.gen import gen_session
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
+
+from StringBot.modules.generate import generate_session, ask_ques, buttons_ques
 
 
-@Bad.on_callback_query(
-    filters.regex(pattern=r"^(gensession|pyrogram|pyrogram1|telethon)$")
-)
-async def cb_choose(_, cq: CallbackQuery):
-    await cq.answer()
-    query = cq.matches[0].group(1)
-    if query == "gensession":
-        return await cq.message.reply_text(
-            text="<b>» ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ғᴏʀ ɢᴇɴᴇʀᴀᴛɪɴɢ ʏᴏᴜʀ sᴇssɪᴏɴ :</b>",
-            reply_markup=gen_key,
-        )
+@Client.on_callback_query(filters.regex(pattern=r"^(generate|pyrogram|pyrogram1|pyrogram_bot|telethon_bot|telethon)$"))
+async def _callbacks(bot: Client, callback_query: CallbackQuery):
+    query = callback_query.matches[0].group(1)
+    if query == "generate":
+        await callback_query.answer()
+        await callback_query.message.reply(ask_ques, reply_markup=InlineKeyboardMarkup(buttons_ques))
     elif query.startswith("pyrogram") or query.startswith("telethon"):
         try:
             if query == "pyrogram":
-                await gen_session(cq.message, cq.from_user.id)
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message)
             elif query == "pyrogram1":
-                await gen_session(cq.message, cq.from_user.id, old_pyro=True)
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, old_pyro=True)
+            elif query == "pyrogram_bot":
+                await callback_query.answer("» ᴛʜᴇ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴡɪʟʟ ʙᴇ ᴏғ ᴩʏʀᴏɢʀᴀᴍ ᴠ2.", show_alert=True)
+                await generate_session(bot, callback_query.message, is_bot=True)
+            elif query == "telethon_bot":
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
             elif query == "telethon":
-                await gen_session(cq.message, cq.from_user.id, telethon=True)
+                await callback_query.answer()
+                await generate_session(bot, callback_query.message, telethon=True)
         except Exception as e:
-            await cq.edit_message_text(e, disable_web_page_preview=True)
+            print(traceback.format_exc())
+            print(e)
+            await callback_query.message.reply(ERROR_MESSAGE.format(str(e)))
+
+
+ERROR_MESSAGE = "ᴡᴛғ ! sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ. \n\n**ᴇʀʀᴏʀ** : {} " \
+            "\n\n**ᴩʟᴇᴀsᴇ ғᴏʀᴡᴀʀᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴛᴏ @PBX_CHAT**, ɪғ ᴛʜɪs ᴍᴇssᴀɢᴇ " \
+            "ᴅᴏᴇsɴ'ᴛ ᴄᴏɴᴛᴀɪɴ ᴀɴʏ sᴇɴsɪᴛɪᴠᴇ ɪɴғᴏʀᴍᴀᴛɪᴏɴ " \
+            "ʙᴇᴄᴀᴜsᴇ ᴛʜɪs ᴇʀʀᴏʀ ɪs **ɴᴏᴛ ʟᴏɢɢᴇᴅ ʙʏ ᴛʜᴇ ʙᴏᴛ** !"
