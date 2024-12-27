e0d9f83427652c72658a0c4dafb74d6b5743d27f
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-27 23:14:46 +0530

    Update __init__.py

--- StringBot/utils/__init__.py
+++ StringBot/utils/__init__.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+#
+# This file is part of < https://github.com/Badhacker98/StringBot > project,
+# and is released under the license agreement specified in:
+# < https://github.com/Badhacker98/StringBot/blob/main/LICENSE >
+#
+# All rights reserved.
+
 from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 
 import config
