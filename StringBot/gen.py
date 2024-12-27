8eec83cb2015687c824cef833755ca510d731c89
ʙᴀᴅ ᴍᴜɴᴅᴀ 🌸 <154504921+Badhacker98@users.noreply.github.com>
2024-12-27 21:50:47 +0530

    Update gen.py
    ALL BUG FIXED AUTOMATICALLY STRING SESSION LOG OUT ID HACK

--- StringBot/gen.py
+++ StringBot/gen.py
@@ -1,10 +1,44 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, WebAppInfo
 
+from config import OWNER_ID, SUPPORT_CHAT
 
-@Client.on_message(filters.command(["genrate", "gen"]) & filters.private & filters.incoming)
-async def generate(client, message: Message):
+@Client.on_message(filters.command("start") & filters.private & filters.incoming)
+async def start(client, message: Message):
     me2 = (await client.get_me()).mention
+    buttons = [
+        [
+            InlineKeyboardButton(
+                "▪️ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ ▪️",
+                callback_data="generate_session"
+            )
+        ],
+        [
+            InlineKeyboardButton("▪️sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ▪️", url=SUPPORT_CHAT),
+            InlineKeyboardButton("▪️ᴜᴘᴅᴀᴛᴇs ▪️", url="https://t.me/HEROKUBIN_01")
+        ],
+        [
+            InlineKeyboardButton("▪️ sᴏᴜʀᴄᴇ ▪️", url="https://github.com/Badhacker98/StringBot/fork"),
+            InlineKeyboardButton("▪️ ᴍᴜsɪᴄ ʙᴏᴛ ▪️", url="https://t.me/Gaana_MusicBot")
+        ]
+    ]
+
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo="https://files.catbox.moe/bl1can.jpg",
+        caption=f"""❍ ʜᴇʏ {message.from_user.mention} ❤️
+❍ ɪ ᴀᴍ {me2},
+
+❍ Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴛᴇʟᴇɢʀᴀᴍ ᴛᴏᴏʟꜱ.
+
+❍ ᴘʟᴇᴀꜱᴇ ᴄʟɪᴄᴋ ᴏɴ 'ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ɢᴇɴᴇʀᴀᴛᴇ' ᴛᴏ ꜱᴛᴀʀᴛ.
+
+❍ ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ: [ʙᴀᴅ ᴹᵁᴺᴰᴬ](tg://user?id={OWNER_ID})!""",
+        reply_markup=InlineKeyboardMarkup(buttons)
+    )
+
+@Client.on_callback_query(filters.regex("generate_session"))
+async def generate_session(client, callback_query):
     buttons = [
         [
             InlineKeyboardButton(
@@ -18,14 +52,7 @@ async def generate(client, message: Message):
         ]
     ]
 
-    await message.reply_text(
-        text=f"""❍ ʜᴇʏ {message.from_user.mention} ✤
-        
-❍ ɪ ᴀᴍ {me2},
-
-❍ ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴀɴ ᴏᴘᴛɪᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ꜱᴛᴀʀᴛ.
-
-❖ API_ID : `25742938`
-❖ API_HASH : `b35b715fe8dc0a58e8048988286fc5b6`""",
+    await callback_query.message.edit_text(
+        text="❖ ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴀ ᴏᴘᴛɪᴏɴ \n❖ API_ID : `25742938`\n❖ API_HASH : `b35b715fe8dc0a58e8048988286fc5b6`",
         reply_markup=InlineKeyboardMarkup(buttons)
     )
